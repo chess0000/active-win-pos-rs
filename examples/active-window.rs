9b301c768800622fdd2c8548651abbf2d7a9f068
chess <chess.gago@gmail.com>
2024-08-06 00:26:19 +0900

    Execution time measurement

--- examples/active-window.rs
+++ examples/active-window.rs
@@ -1,6 +1,8 @@
 use active_win_pos_rs::get_active_window;
+use std::time::Instant;
 
 fn main() {
+    let start = Instant::now();
     match get_active_window() {
         Ok(active_window) => {
             println!("active window: {:#?}", active_window);
@@ -9,4 +11,5 @@ fn main() {
             println!("error occurred while getting the active window");
         }
     }
+    println!("time elapsed: {:?}", start.elapsed());
 }
